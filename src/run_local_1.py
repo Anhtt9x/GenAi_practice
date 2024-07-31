47dc9ec99e921b7bd6091716bb4dd8d6677a8f93
Anhtt9x <anhtt454598@gmail.com>
2024-07-31 22:55:21 +0700

    llm run

--- src/run_local_1.py
+++ src/run_local_1.py
@@ -10,16 +10,16 @@ B_SYS , E_SYS = "<<SYS>>\n" , "\n<<SYS>>\n\n"
 
 instruction = "Convert the follow text from English to French: \n\n {text}"
 
-SYSTEM_PROMPT = B_SYS + DEFAULT_SYSTEM_PROMPT + E_SYS
+SYSTEM_PROMPT = B_SYS + CUSTOM_SYSTEM_PROMPT + E_SYS
 
 template = B_INST + SYSTEM_PROMPT + instruction + E_INST
 
 prompt = PromptTemplate(template=template, input_variables=["text"])
 
-llm = CTransformers(model="model/llama-2-7b-chat.ggmlv3.q2_K.bin", model_type="llama",
+llm = CTransformers(model="model/llama-2-7b-chat.ggmlv3.q8_0.bin", model_type="llama",
                     config={'max_new_tokens':128, 'temperature':0.7}
                     )
 
 chains = LLMChain(llm=llm, prompt=prompt)
 
-print(chains.invoke("Hello"))
+print(chains.invoke("How are you ?"))
